35ba828fffc2e1ef8df5d656771f3c8b215f7cb9
Noctus Rex <noxjudex@gmail.com>
2025-05-30 17:35:31 +0200

    Loading indicator and cache games

--- src/app/services/firebase-storage.service.ts
+++ src/app/services/firebase-storage.service.ts
@@ -12,14 +12,16 @@ import {
   setDoc
 } from '@angular/fire/firestore';
 import {Report} from "../models/report.model";
-import {BehaviorSubject, concatMap, from, map, Observable, take} from "rxjs";
+import {BehaviorSubject, concatMap, finalize, from, map, Observable, take, tap} from "rxjs";
 import {inject, Injectable, Injector, runInInjectionContext} from "@angular/core";
 import {Auth, signInAnonymously} from "@angular/fire/auth";
+import {LoadingController} from "@ionic/angular";
 
 
 @Injectable()
 export class FireBaseStorageService extends StorageService {
 
+  private loadingController = inject(LoadingController);
   private injector = inject(Injector);
   private firestore = inject(Firestore);
   private auth = inject(Auth);
@@ -33,22 +35,26 @@ export class FireBaseStorageService extends StorageService {
   }
 
   override getGames(): Observable<Array<Game>> {
-    return runInInjectionContext(this.injector, () => {
+    if (this.games.value && this.games.value.length > 0) {
+      return this.games.pipe(take(1));
+    }
+
+    return this.runInContext(() => {
       const gamesRef = collection(this.firestore, 'games');
 
       return collectionData(gamesRef, {idField: 'id'}) as Observable<Array<Game>>;
-    }).pipe(take(1));
+    }).pipe(take(1), tap(games => this.games.next(games)));
   }
 
   override getReports(): Observable<Array<Report>> {
-    return runInInjectionContext(this.injector, () => {
+    return this.runInContext(() => {
       const reportsRef = collection(this.firestore, 'reports');
       return (collectionData(reportsRef, {idField: 'id'}) as Observable<Report[]>).pipe(take(1));
     });
   }
 
   override getReport(id: number): Observable<Report | undefined> {
-    return runInInjectionContext(this.injector, () => {
+    return this.runInContext(() => {
       const reportsRef = doc(this.firestore, `reports/${id}`);
 
       return (docData(reportsRef, {idField: 'id'}) as Observable<Report>).pipe(take(1));
@@ -56,11 +62,11 @@ export class FireBaseStorageService extends StorageService {
   }
 
   override deleteReport(id: number): Observable<void> {
-    return from(runInInjectionContext(this.injector, () => {
+    return this.runInContext(() => {
       const reportRef = doc(this.firestore, `reports/${id}`);
 
-      return deleteDoc(reportRef);
-    }));
+      return from(deleteDoc(reportRef));
+    });
   }
 
   override setReports(reports: Array<Report>): Observable<void> {
@@ -70,21 +76,21 @@ export class FireBaseStorageService extends StorageService {
   override addOrUpdateReport(report: Report): Observable<void> {
     return this.getHighestReportId().pipe(
       concatMap(lastId => {
-        return from(runInInjectionContext(this.injector, () => {
+        return this.runInContext(() => {
           if (report.id === undefined) {
             report.id = (lastId ?? 0) + 1;
           }
 
           const reportRef = doc(this.firestore, `reports/${report.id}`);
 
-          return setDoc(reportRef, report);
-        }));
+          return from(setDoc(reportRef, report));
+        });
       })
     )
   }
 
   getHighestReportId(): Observable<number | null> {
-    return runInInjectionContext(this.injector, () => {
+    return this.runInContext(() => {
       const reportsCollection = collection(this.firestore, 'reports');
       const q = query(
         reportsCollection,
@@ -103,5 +109,20 @@ export class FireBaseStorageService extends StorageService {
         })
       );
     });
+  }
+
+  private runInContext(fn: () => Observable<any>): Observable<any> {
+    return from(this.loadingController.create({
+      duration: 30000,
+      keyboardClose: true,
+      showBackdrop: true,
+      spinner: "crescent"
+    })).pipe(
+      concatMap(loading => from(loading.present()).pipe(
+          concatMap(() => from(runInInjectionContext(this.injector, () => fn()))),
+          finalize(() => loading.dismiss())
+        )
+      )
+    );
   }
 }
